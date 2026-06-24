e9d9186a4c1f2a70866a2956a430c423f3331422
Galang Sopyan <galangsopyan05@gmail.com>
2026-06-25 01:35:32 +0700

    Rename load_idx_stocks to load_stocks and simplify

--- utils/stock_list.py
+++ utils/stock_list.py
@@ -3,16 +3,10 @@ import streamlit as st
 
 
 @st.cache_data(ttl=86400)
-def load_idx_stocks():
+def load_stocks():
 
     try:
+        return pd.read_csv("data/idx_stocks.csv")
 
-        url = "https://raw.githubusercontent.com/datasets/s-and-p-500-companies/master/data/constituents.csv"
-
-        df = pd.read_csv(url)
-
-        return df
-
-    except Exception:
-
+    except:
         return pd.DataFrame()
